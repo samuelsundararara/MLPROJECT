b812566d9ad0e83799e23201d0c6cb99aea9bfcf
samuelsundararara <75292813+samuelsundararara@users.noreply.github.com>
2020-12-01 12:40:11 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Rx8okd9JcAJeVNnpgqJrYaSfC5FA_Hz7'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-qE7Z1Z4htGkgY4h0kmZdPTCRZ376wYw'
 export_file_name = 'export.pkl'
 
 classes = ['cataract', 'glaucoma', 'normal', 'retina_disease']
